990d12be0a4e0346a00802b27b7848f2c0a47da0
AJAY MORE <ajaymore123.indore@gmail.com>
2024-01-18 13:09:34 +0530

    added home route /

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
 app.post("/register", async (req, res) => {
   // console.log(req.body);
   // res.status(200).json(req.body);
